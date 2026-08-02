c7f0fe89415f3a48ac3dda5038238739372f7d14
TimurRakhimov <lollipopnote8@gmail.com>
2026-08-02 15:21:41 +0300

    update: add LETO_HANDLE for font, screen and bitmap items, refactror CMakeLists, CMakePresets

--- include/LetoAPI_V1/LetoAPI_V1_Def.h
+++ include/LetoAPI_V1/LetoAPI_V1_Def.h
@@ -1,10 +1,38 @@
 #pragma once
 
+#define LETO_HANDLE(name) typedef struct name##__ { int leto_handle; } name
+
 /// Opaque pointer to Font instance
-typedef struct LetoFont_V1 LetoFont_V1;
+LETO_HANDLE(LetoFont_V1);
 
 /// Opaque pointer to Screen instance
-typedef struct LetoScreen_V1 LetoScreen_V1;
+LETO_HANDLE(LetoScreen_V1);
 
 /// Opaque pointer to Bitmap instance
-typedef struct LetoBitmap_V1 LetoBitmap_V1;
+LETO_HANDLE(LetoBitmap_V1);
+
+// C++ system realisation
+
+#ifdef __cplusplus
+
+template <typename T_Derived, typename T_Handle>
+class LetoHandleImpl {
+public:
+    static T_Derived* FromHandle(T_Handle* h) {
+        return reinterpret_cast<T_Derived*>(h);
+    }
+    
+    static const T_Derived* FromHandle(const T_Handle* h) {
+        return reinterpret_cast<const T_Derived*>(h);
+    }
+
+    static T_Handle* ToHandle(T_Derived* d) {
+        return reinterpret_cast<T_Handle*>(d);
+    }
+
+    static const T_Handle* ToHandle(const T_Derived* d) {
+        return reinterpret_cast<const T_Handle*>(d);
+    }
+};
+
+#endif
